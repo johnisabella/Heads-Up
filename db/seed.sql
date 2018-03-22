3de2e01c8bf942b076b8d8ffccf8780e2ed09756
Cesar Pocai <c_pocai1@hotmail.com>
2018-03-22 18:10:22 -0400

    Heads-Up 1

--- db/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO messages_tbls (Title, Message) VALUES ("EmergencyAlert", "This is an Emergency Alert Test.");
-INSERT INTO messages_tbls (Title, Message) VALUES ("CampusNewsAlert", "This is an Campus News Alert Test.");
-INSERT INTO messages_tbls (Title, Message) VALUES ("AreaNewsAlert", "This is an Area News Alert Test.");
-INSERT INTO messages_tbls (Title, Message) VALUES ("SchoolClosureAlert", "This is an School Closure Alert Test.");
